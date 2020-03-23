52557dc7e6c1a5691c7765d2455ea2783841f925
Neha Gehani <nehapgehani@gmail.com>
2020-03-24 04:21:22 +0530

    chore: using custom page props for app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,18 @@ import "../assets/styles/index.scss";
 import React from "react";
 import { AppProps } from "next/app";
 import Header from "../components/global/Header";
+import { NextPage } from "next";
 
-function App({ Component, pageProps }: AppProps) {
+export interface LayoutPageProps {
+  className?: string;
+}
+
+interface IProps {
+  Component?: NextPage<LayoutPageProps>;
+  pageProps: LayoutPageProps;
+}
+
+function App({ Component, pageProps }: IProps) {
   return (
     <div className="h-100">
       <Header />
