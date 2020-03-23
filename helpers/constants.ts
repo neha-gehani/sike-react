4692c01d2c51f9881e3b979ffbc0aee08d250f9f
Neha Gehani <nehapgehani@gmail.com>
2020-03-24 02:30:02 +0530

    feat: added a constants file

---
+++ helpers/constants.ts
@@ -0,0 +1,2 @@
+export const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInJvbGUiOiJndWVzdCIsImlhdCI6MTU4NDk5NjQ5OSwiZXhwIjoxNjE2NTU0MDk5fQ.0gDPblo-Zp4-x66JFeNfvd4NNyuUEgHbX_GxvAInzTU";
