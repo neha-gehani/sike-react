e8dc5f6defa17828c69cd52a31953432b95ce6a9
Hiranandani, Himanshu <hhiranandani@DELOITTE.com>
2020-03-24 14:40:12 +0530

    removes extra params

--- api/game.ts
+++ api/game.ts
@@ -2,25 +2,25 @@ import { httpClient } from "./httpClient";
 import { Game } from "./interface";
 
 export const createGame = async (): Promise<Game> => {
-  const result = await httpClient.post<Game>("/game", {});
+  const result = await httpClient.post<Game>("/game");
 
   return result;
 };
 
 export const joinGame = async (gameId): Promise<Game> => {
-  const result = await httpClient.post<Game>(`/game/${gameId}/join`, {});
+  const result = await httpClient.post<Game>(`/game/${gameId}/join`);
 
   return result;
 };
 
 export const startGame = async (gameId): Promise<Game> => {
-  const result = await httpClient.post<Game>(`/game/${gameId}/start`, {});
+  const result = await httpClient.post<Game>(`/game/${gameId}/start`);
 
   return result;
 };
 
 export const getGame = async (gameId): Promise<Game> => {
-  const result = await httpClient.get<Game>(`/game/${gameId}`, {});
+  const result = await httpClient.get<Game>(`/game/${gameId}`);
 
   return result;
 };
