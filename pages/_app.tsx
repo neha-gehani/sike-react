31d391adeee031ff37238885e495df722394492a
Hiranandani, Himanshu <hhiranandani@DELOITTE.com>
2020-03-29 19:43:25 +0530

    Adds PWA setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "../assets/styles/index.scss";
 import Header from "../components/global/Header";
 import { NextPage } from "next";
-import Router from "next/router";
+import Head from "next/dist/next-server/lib/head";
 import withRedux from "next-redux-wrapper";
 import { store } from "../store";
 import { Provider } from "react-redux";
-import AuthenticatedRoute from "../components/global/AuthenticatedRoute";
-import { useEffect } from "react";
 
 export interface LayoutPageProps {
   className?: string;
@@ -22,7 +20,21 @@ function App({ Component, pageProps, store }: IProps) {
   
   return (
     <Provider store={store}>
-      <div className="app-container">
+       <Head>
+        <title>Sike</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='Sike game' />
+        <meta name='keywords' content='sike, game, questions, friends' />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#6900ff"/>
+      </Head>
+      <div className="app-container"> 
         <Header />
         <Component {...pageProps} />
       </div>
